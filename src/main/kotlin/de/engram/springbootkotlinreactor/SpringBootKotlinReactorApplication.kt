03be2aec589e20078910f750ce18336a3a5cbdd9
Erik Legenhausen <erik.legenhausen@engram.de>
2021-08-17 23:27:35 +0200

    added accept header handling

--- src/main/kotlin/de/engram/springbootkotlinreactor/SpringBootKotlinReactorApplication.kt
+++ src/main/kotlin/de/engram/springbootkotlinreactor/SpringBootKotlinReactorApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.server.coRouter
 
 @SpringBootApplication
@@ -17,7 +18,9 @@ fun main(args: Array<String>) {
 class RouterConfiguration {
     @Bean
     fun routes(requestHandler: RequestHandler) = coRouter {
-        GET("/", requestHandler::root)
-        GET("/hello", requestHandler::hello)
+		accept(APPLICATION_JSON).nest {
+			GET("/", requestHandler::root)
+			GET("/hello", requestHandler::hello)
+		}
     }
 }
