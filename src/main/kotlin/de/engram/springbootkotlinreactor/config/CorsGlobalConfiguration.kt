3bd5dae18d10100e5d0be811e1e1c60796ac3e27
Erik Legenhausen <erik.legenhausen@engram.de>
2021-09-08 22:27:03 +0200

    CORS support

---
+++ src/main/kotlin/de/engram/springbootkotlinreactor/config/CorsGlobalConfiguration.kt
@@ -0,0 +1,15 @@
+package de.engram.springbootkotlinreactor.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class CorsGlobalConfiguration : WebFluxConfigurer {
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "PUT", "POST", "DELETE")
+			.allowedHeaders("Authorization")
+    }
+}
